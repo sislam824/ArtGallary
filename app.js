e465b553d8252ff02abd55d9d3f71c6b895be95e
Abu Osama <s.islam20000@gmail.com>
2024-07-04 21:20:13 +0530

    cors updated

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const { connectDB } = require('./config/db');
-const productRoutes = require('./routes/productRoutes');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const errorMiddleware = require('./middleware/errorMiddleware');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const { connectDB } = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const errorMiddleware = require("./middleware/errorMiddleware");
+const path = require("path");
 
 const app = express();
 
@@ -18,10 +19,10 @@ app.use(express.json());
 app.use(cors());
 
 // Define Routes
-app.use('/api/products', productRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 // Error Middleware
 app.use(errorMiddleware);
